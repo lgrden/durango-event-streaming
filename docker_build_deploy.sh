97c7233ed2764ca371477511afc85554e763a5d1
lgrden <lgrden@gmail.com>
2021-01-29 12:58:02 +0100

    docker env

--- docker_build_deploy.sh
+++ docker_build_deploy.sh
@@ -2,4 +2,4 @@
 mvn clean install
 docker build -t durango-event-consumer event-consumer
 docker build -t durango-event-producer event-producer
-docker-compose -f docker-compose-all.yaml up -d
+docker-compose -f docker-compose-all.yaml --env-file docker.env up -d
